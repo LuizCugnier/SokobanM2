5f83c6a9c1f986414ddb0a9f3acc2bfaff6f79b2
LuizCugnier <luizcugnier.neto@gmail.com>
2023-05-07 19:51:43 -0300

    Atualizacao SokobanM2

--- config.h
+++ config.h
@@ -14,26 +14,78 @@ struct Menu{
         cout << "[3] Sobre\n";
         cout << "[4] Fim\n";
     }
+
 };
+
+struct Jogador{
+        int x, y;
+    };
 
 struct Input{
     char tecla;
     char opcao_menu;
+
+    void input(Menu &menu, Jogador* jogador, int (&mj)[13][13]){
+        if (_kbhit()){
+            tecla = _getch();
+            switch (tecla)
+            {
+            case 72: case 'w': //cima
+                if (jogador->x > 0 && mj[jogador->x - 1][jogador->y] == 0){
+                    jogador->x--;
+                } else if (mj[jogador->x - 1][jogador->y] == 2 && mj[jogador->x - 2][jogador->y] != 2 && mj[jogador->x - 2][jogador->y] != 1 ){
+                    mj[jogador->x - 2][jogador->y] = 2;
+                    mj[jogador->x - 1][jogador->y] = 0;
+                    jogador->x--;
+                }
+                break;
+            case 80: case 's': //baixo
+                if (jogador->x > 0 && mj[jogador->x + 1][jogador->y] == 0){
+                    jogador->x++;
+                } else if (mj[jogador->x + 1][jogador->y] == 2 && mj[jogador->x + 2][jogador->y] != 2 && mj[jogador->x + 2][jogador->y] != 1){
+                    mj[jogador->x + 2][jogador->y] = 2;
+                    mj[jogador->x + 1][jogador->y] = 0;
+                    jogador->x++;
+                }
+                break;
+            case 75: case 'a': //esquerda
+                if (jogador->y > 0 && mj[jogador->x][jogador->y - 1] == 0){
+                    jogador->y--;
+                } else if (mj[jogador->x][jogador->y - 1] == 2 && mj[jogador->x][jogador->y - 2] != 2 && mj[jogador->x][jogador->y - 2] != 1){
+                    mj[jogador->x][jogador->y - 2] = 2;
+                    mj[jogador->x][jogador->y - 1] = 0;
+                    jogador->y--;
+                }
+                break;
+            case 77: case 'd': //direita
+                if (jogador->y > 0 && mj[jogador->x][jogador->y + 1] == 0){
+                    jogador->y++;
+                } else if (mj[jogador->x][jogador->y + 1] == 2 && mj[jogador->x][jogador->y + 2] != 2 && mj[jogador->x][jogador->y + 2] != 1){
+                    mj[jogador->x][jogador->y + 2] = 2;
+                    mj[jogador->x][jogador->y + 1] = 0;
+                    jogador->y++;
+                }
+                break;
+            case '0': //volta menu
+                menu.termina_jogo = false;
+                system("cls");
+                menu.imprime_menu();
+                break;
+            default:
+                break;
+            }
+        }
+    }
 };
 
-    struct Jogador{
-        int *x, *y;
-    };
-
 struct Mapa{
-    int mj;
 
-    void imprime_mapa(Jogador jogador, int (&mj)[13][13]){
+    void imprime_mapa(Jogador* jogador, int (&mj)[13][13]){
 
         //Imprime o jogo: mapa e personagem.
         for (int i = 0; i < 13; i++) {
             for (int j = 0; j < 13; j++) {
-                if (i == jogador.x && j == jogador.y) {
+                if (i == jogador->x && j == jogador->y) {
                     cout << char(2); //personagem
                 }
                 
@@ -55,4 +107,21 @@ struct Mapa{
 
         cout << "\nAperte '0' para voltar";
     }
+};
+
+struct Jogo{
+
+    void novo_jogo(COORD coord, Mapa mapa, Input input, Jogador* jogador, Menu& menu, int (&mj)[13][13]){
+        system("cls");
+        menu.termina_jogo = true;
+
+        while(menu.termina_jogo){
+            //Posiciona no inicio do console
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+            mapa.imprime_mapa(jogador, mj);
+            input.input(menu, jogador, mj);
+        }
+        
+    };
 };
--- loop_principal.h
+++ loop_principal.h
@@ -1,37 +1,45 @@
 using namespace std;
-struct Jogo{
+void loop_principal(Menu menu, Input input, COORD coord, Mapa mapa, Jogador* jogador, Jogo jogo, int (&mj)[13][13]){
+    while (menu.principal){
+        
+        if (_kbhit()){
+            input.tecla = _getch();
 
-    void jogo(Menu menu, Input input, Mapa mapa, Mapa &mj, Jogador jogador, COORD coord){
-        while (menu.principal){
-            
-            if (_kbhit()){
-                input.tecla = _getch();
+            switch (input.tecla)
+            {
+            case '0':
+                //imprimir o menu novamente
+                menu.imprime_menu();
+                break;
+            case '1':
+                //cria um novo jogo
+                //jogo.novo_jogo(mapa, input, jogador, menu, mj);
+                system("cls");
+                menu.termina_jogo = true;
 
-                switch (input.tecla)
-                {
-                case '0':
-                    //imprimir o menu novamente
-                    menu.imprime_menu();
-                    break;
-                case '1':
-                    //cria um novo jogo
-                    
-                    break;
-                case '2':
-                    //continua um jogo
-                    
-                    break;
-                case '3':
-                    //informações do jogo
-                    break;
-                case '4':
-                    //termina jogo
-                    menu.principal = false;
-                    break;
-                default:
-                    break;
+                while(menu.termina_jogo){
+                    //Posiciona no inicio do console
+                    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+                    mapa.imprime_mapa(jogador, mj);
+                    input.input(menu, jogador, mj);
                 }
+                break;
+            case '2':
+                //continua um jogo
+                
+                break;
+            case '3':
+                //informações do jogo
+                break;
+            case '4':
+                //termina jogo
+                menu.principal = false;
+                break;
+            default:
+                break;
             }
         }
     }
-};
+}
+
